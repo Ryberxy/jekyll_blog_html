4c28327d2384cc720aa74f2db3fe9cc7ce0442cc
Ryberxy <gagomartin2006@gmail.com>
2025-10-21 18:43:36 +0200

    Actualización automática

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1761064720',resources: [
+      '/jekyll_blog_html/assets/css/jekyll-theme-chirpy.css',
+      '/jekyll_blog_html/',
+      
+        '/jekyll_blog_html/categories/',
+      
+        '/jekyll_blog_html/tags/',
+      
+        '/jekyll_blog_html/archives/',
+      
+        '/jekyll_blog_html/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
