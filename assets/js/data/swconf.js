6f71c10060245cad3fb7fb9fc1f91ab3f9b54a6e
Ryberxy <gagomartin2006@gmail.com>
2025-10-30 12:51:08 +0100

    Actualización

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761824303',resources: [
+    cacheName: 'chirpy-1761825042',resources: [
       '/jekyll_blog_html/assets/css/jekyll-theme-chirpy.css',
       '/jekyll_blog_html/',
       
